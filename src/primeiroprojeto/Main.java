4c9b8c36a47ba9e79d16b403c07429def5e3da1f
claudeny avelino <claudeny.avelino@hotmail.com>
2023-10-04 00:33:14 -0300

    "Trabalhando com texto"

--- src/primeiroprojeto/Main.java
+++ src/primeiroprojeto/Main.java
@@ -1,9 +1,25 @@
 package primeiroprojeto;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ola mundo JAVA");
+        System.out.println("Esse é o Scree Match");
+        System.out.println("Filme: Tp Gun: Maverick");
+
+        int anoDeLacamento = 2022;
+        System.out.println("Ano de Lançamento: "+anoDeLacamento);
+        boolean incluidoNoPlano = true;
+
+        double media = (9.8+6.3+8)/3;
+        System.out.println(media);
+       String sinopse = """
+                Filme TOP Gun
+                Fime de aventura com galã dos anos 80
+                Muito bom
+                Ano de lançamento
+                """ +anoDeLacamento;
+        System.out.println(sinopse);
+
+
+
     }
 }
